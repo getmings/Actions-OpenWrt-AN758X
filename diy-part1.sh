2fdd9da5032d18800fc65fc82ac1cc4c8cb4ea36
getmings <getming@outlook.com>
2026-09-25 21:22:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -163,12 +163,18 @@ fi
 # --- luci-app-zzzcatspeedtest ---
 clone https://github.com/Yuzhii0718/luci-app-zzzcatspeedtest "$PKG_DIR/luci-app-zzzcatspeedtest" main
 
+# --- luci-app-quickfile 文件管理器---
+clone https://github.com/sbwml/luci-app-quickfile "$PKG_DIR/luci-app-quickfile"
+
 # --- VPN easytier  ---
 clone https://github.com/EasyTier/luci-app-easytier "$PKG_DIR/luci-app-easytier"
 
 # --- 主题 aurora ---
 clone https://github.com/eamonxg/luci-theme-aurora "$PKG_DIR/luci-theme-aurora"
 clone https://github.com/eamonxg/luci-app-aurora-config "$PKG_DIR/luci-app-aurora-config"
+
+# --- luci-theme-shadcn ---
+clone https://github.com/eamonxg/luci-theme-shadcn "$PKG_DIR/luci-theme-shadcn"
 
 # ---------------------------------------------------------
 # 校验：默认开启的两个插件必须拉到，否则 defconfig 会静默剔除，
